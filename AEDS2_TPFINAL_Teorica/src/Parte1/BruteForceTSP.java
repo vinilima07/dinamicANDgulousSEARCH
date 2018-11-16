8cff4e74203add8d962888e059ca3f6f48539581
Vinicius Franca <vinilima07@gmail.com>
2018-11-16 09:49:27 -0200

    Up project brute force

---
+++ AEDS2_TPFINAL_Teorica/src/Parte1/BruteForceTSP.java
@@ -0,0 +1,182 @@
+package Parte1;
+
+/*
+ * Problem Statement: 	Solve traveling salesman problem using brute force search 
+ */
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class BruteForceTSP {
+
+	/*
+	 * shortestPath:		shortest path to visit all cities exactly once.
+	 * shortestDistance:	variable will store distance to traverse shortestPath.
+	 * longestDistance:		variable will contain longest distance to visit all cities.
+	 *  
+	 */
+	public static int shortestDistance;
+	public static int longestDistance;
+	public static ArrayList<Integer> shortestPath;
+	
+	/*
+	 * Reads input file; solves the traveling salesman problem using brute force search; writes the output file
+	 * 
+	 * @param	pathName	path of input file.
+	 * */
+	public static void startTSP(String pathName) {
+		File file = new File("output.txt");
+		try {
+                File f = new File(pathName);
+                Scanner s = new Scanner(f);
+                int numCidades = Integer.parseInt(s.nextLine());
+                int[][] adjacency_matrix = new int[numCidades][numCidades];
+                String adjacencias = "";
+                int linha = 0;
+                int coluna = 0;
+
+                while(s.hasNext()) {
+                    adjacencias = s.nextLine();
+                    Scanner dist = new Scanner(adjacencias).useDelimiter(" ");
+                    while (dist.hasNext()){
+                        adjacency_matrix[linha][coluna] = dist.nextInt();
+                        coluna++;
+                    }
+                    coluna = 0;
+                    linha++;
+                }
+                int problemNo = 0;
+                shortestDistance = Integer.MAX_VALUE;
+                longestDistance = Integer.MIN_VALUE;
+                shortestPath = null;
+                int totalCities = numCidades;
+                long startTime = System.currentTimeMillis();
+                solveTSP(adjacency_matrix);
+                long endTime = System.currentTimeMillis();
+                
+                for(int city : shortestPath){
+                    System.out.print((Integer.toString(city))+"->");
+                }
+                System.out.println("0");
+                
+		} catch(FileNotFoundException expception) {
+
+			System.out.println("File given does not exists.");
+			expception.printStackTrace();
+			System.exit(0);
+		}
+		catch(IOException ioException){
+			System.out.println("Error occured during Input Output operation.");
+			ioException.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	/*
+	 * Solves the traveling salesman problem for given input
+	 * 
+	 * @param	distanceMatrix	stores the distance between two cities.
+	 * */
+	private static void solveTSP(int[][] distanceMatrix) {
+
+		int totalCities = distanceMatrix.length;
+		ArrayList<Integer> cities = new ArrayList<Integer>();
+		for(int i=0;i<totalCities;i++){
+			cities.add(i);
+		}
+		int startCity = cities.get(0);
+		int currentDistance = 0;
+		bruteForceSearch(distanceMatrix, cities, startCity, currentDistance);
+	}
+	/*
+	 * Applies brute force algorithm to generate all possible permutations of journey.
+	 * 
+	 * @param	distanceMatrix	stores the distance between two cities.
+	 * @param	cities			possible path of a journey.
+	 * @param	startCity		starting city of the journey.
+	 * @param	currentDistance	distance covered in the journey.
+	 * */
+	
+	private static void bruteForceSearch(int[][] distanceMatrix, ArrayList<Integer> cities, int startCity, int currentDistance) {
+
+		if(startCity < cities.size()-1){
+			for(int i=startCity; i < cities.size(); i++){
+				int tempCity = cities.get(i);
+				cities.set(i, cities.get(startCity));
+				cities.set(startCity, tempCity);
+				currentDistance = computeDistance(cities,distanceMatrix);
+				bruteForceSearch(distanceMatrix, cities, startCity+1, currentDistance);
+				tempCity = cities.get(i);
+				cities.set(i, cities.get(startCity));
+				cities.set(startCity, tempCity);
+			}
+		}
+		else{
+			if(shortestDistance > currentDistance){
+				shortestDistance = currentDistance;
+				shortestPath = new ArrayList<Integer>(cities);
+			}
+			if(longestDistance < currentDistance){
+				longestDistance = currentDistance;
+			}
+		}
+	}
+
+	/*
+	 * Computes the distance covered during the journey
+	 * 
+	 * @param	cities			possible path of the journey.
+	 * @param	distanceMatrix	stores distance between two cities.
+	 * 
+	 * */
+	private static int computeDistance(ArrayList<Integer> cities, int[][] distanceMatrix) {
+		int distance = 0;
+		for(int i=0;i<cities.size()-1;i++){
+			distance = distance + distanceMatrix[cities.get(i)][cities.get(i+1)];
+		}
+		distance = distance + distanceMatrix[cities.get(cities.size()-1)][cities.get(0)];
+		return distance;
+	}
+        
+        /*
+        * Gera matrizes de adjacencias e as escrevem nos arquivos de teste 
+        *
+        * */
+        public static void generateAdj(int numCity) throws IOException{
+            Random r = new Random();
+            FileWriter fileWriter = new FileWriter("src\\Parte1\\adjacencias"+numCity+".txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.printf("%d\n", numCity);
+            for(int i=0; i < numCity ; i++){
+                for(int j = 0; j < numCity ; j++){
+                    if(j != i)  printWriter.printf("%d ", 1+r.nextInt(50));
+                    else  printWriter.print("0 ");
+                }
+                printWriter.printf("\n");
+            }
+            fileWriter.close();
+        }
+	
+	public static void main(String[] args) throws IOException {
+//            Scanner s = new Scanner(System.in);
+//            System.out.println("Insira o numero de cidades");
+//             s.nextInt()
+
+        String cwd = System.getProperty("user.dir");
+        System.out.println("Current working directory : " + cwd);
+            
+            for(int i = 2; i < 12 ; i+=2){
+                generateAdj(i);
+                //startTSP("Parte1\\adjacencias"+i+".txt");
+                System.out.println("\n");
+            }
+            
+	}
+}
